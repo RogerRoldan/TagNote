973fe77d014e7818f5be337828269d6d5c202e9d
jitjat123 <santiagomartinez760@gmail.com>
2023-12-06 10:06:16 -0500

    Updated Task model var group now is delete in cascade OnUpdate and OnDelete

--- models/Task.go
+++ models/Task.go
@@ -1,10 +1,10 @@
 package models
 
 type Task struct {
-	ID           uint   `gorm:"primaryKey,autoIncrement" json:"id"`
+	ID           uint   `gorm:"primaryKey;autoIncrement:true" json:"id"`
 	Title        string `gorm:"not null" json:"title"`
 	Description  string `gorm:"not null" json:"description"`
 	State        string `gorm:"not null" json:"state"`
-	Group        Group  `gorm:"foreignKey:ID" json:"group_id"`
+	Group        Group  `gorm:"foreignKey:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"group_id"`
 	TaskAssigned []Task `gorm:"many2many:task_assigned" json:"task_assigned"`
 }
