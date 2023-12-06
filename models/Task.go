5604d28f5fcae7ead1a5fd31302986fbfa60736c
jitjat123 <santiagomartinez760@gmail.com>
2023-12-06 01:16:17 -0500

    autoIncrement models

--- models/Task.go
+++ models/Task.go
@@ -5,6 +5,6 @@ type Task struct {
 	Title        string `gorm:"not null" json:"title"`
 	Description  string `gorm:"not null" json:"description"`
 	State        string `gorm:"not null" json:"state"`
-	Group        Group  `gorm:"foreignKey:ID,not null" json:"group_id"`
+	Group        Group  `gorm:"foreignKey:ID" json:"group_id"`
 	TaskAssigned []Task `gorm:"many2many:task_assigned" json:"task_assigned"`
 }
