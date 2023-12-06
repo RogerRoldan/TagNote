c380ca650063e899f217abd1683827a67f368b29
jitjat123 <santiagomartinez760@gmail.com>
2023-12-06 01:15:03 -0500

    autoIncrement models

--- models/Group.go
+++ models/Group.go
@@ -1,7 +1,7 @@
 package models
 
 type Group struct {
-	ID                 uint   `gorm:"primaryKey" json:"id"`
+	ID                 uint   `gorm:"primaryKey,autoIncrement" json:"id"`
 	Title              string `gorm:"not null" json:"title"`
 	Description        string `gorm:"not null" json:"description"`
 	GroupsUser         []User `gorm:"many2many:group_user" json:"groups_user"`
--- models/Task.go
+++ models/Task.go
@@ -1,10 +1,10 @@
 package models
 
 type Task struct {
-	ID           uint   `gorm:"primaryKey" json:"id"`
+	ID           uint   `gorm:"primaryKey,autoIncrement" json:"id"`
 	Title        string `gorm:"not null" json:"title"`
 	Description  string `gorm:"not null" json:"description"`
 	State        string `gorm:"not null" json:"state"`
-	Group        Group  `gorm:"foreignKey:ID" json:"group_id"`
+	Group        Group  `gorm:"foreignKey:ID,not null" json:"group_id"`
 	TaskAssigned []Task `gorm:"many2many:task_assigned" json:"task_assigned"`
 }
