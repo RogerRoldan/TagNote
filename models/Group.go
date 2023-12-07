e4e9eac0779e0be35774099faf38cfc9c2a174f5
jitjat123 <santiagomartinez760@gmail.com>
2023-12-06 19:21:58 -0500

    Fix foreing key task in group

--- models/Group.go
+++ models/Group.go
@@ -1,9 +1,10 @@
 package models
 
 type Group struct {
-	ID                 uint   `gorm:"primaryKey;autoIncrement:true"json:"id"`
+	ID                 uint   `gorm:"primaryKey;autoIncrement:true" json:"id"`
 	Title              string `gorm:"not null" json:"title"`
 	Description        string `gorm:"not null" json:"description"`
+	Task               []Task `gorm:"foreignKey:ID;gorm:constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"task"`
 	GroupsUser         []User `gorm:"many2many:group_user" json:"groups_user"`
 	AdministratorGroup []User `gorm:"many2many:administrator_group" json:"administrator_group"`
 }
--- models/Task.go
+++ models/Task.go
@@ -5,6 +5,5 @@ type Task struct {
 	Title        string `gorm:"not null" json:"title"`
 	Description  string `gorm:"not null" json:"description"`
 	State        string `gorm:"not null" json:"state"`
-	Group        Group  `gorm:"foreignKey:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"group_id"`
 	TaskAssigned []Task `gorm:"many2many:task_assigned" json:"task_assigned"`
 }
