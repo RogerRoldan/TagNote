adc9231cfd4ea0269341c4560dc859bd2888574a
jitjat123 <santiagomartinez760@gmail.com>
2023-12-11 01:36:41 -0500

    Se crearon las credenciales aparte

--- Backend/infraestructure/conectionDatabase.go
+++ Backend/infraestructure/conectionDatabase.go
@@ -1,19 +1,45 @@
 package infraestructure
 
 import (
+	"encoding/json"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+type Credentials struct {
+	Host     string `json:"host"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	DBName   string `json:"dbname"`
+	Port     string `json:"port"`
+	SSLMode  string `json:"sslmode"`
+	TimeZone string `json:"timezone"`
+}
+
 // function return connection database
 func GetConnection() *gorm.DB {
+	// Open JSON file
+	file, err := os.Open("C:\\Users\\PC\\Desktop\\Santiago\\GO\\Workhub\\Backend\\credentials.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-	var stringConnection = "host=localhost user=postgres password=root dbname=workhub port=5432 sslmode=disable TimeZone=America/Bogota"
-	//var stringConnection = "host=localhost user=postgres password=postgres dbname=workhub port=5433 sslmode=disable TimeZone=America/Bogota"
+	// Decode JSON data
+	var creds Credentials
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&creds)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create connection string
+	stringConnection := "host=" + creds.Host + " user=" + creds.User + " password=" + creds.Password + " dbname=" + creds.DBName + " port=" + creds.Port + " sslmode=" + creds.SSLMode + " TimeZone=" + creds.TimeZone
+
 	db, err := gorm.Open(postgres.Open(stringConnection), &gorm.Config{})
-
 	if err != nil {
 		panic("failed to connect database")
 	}
