625a904869fa8004ac4933ad761f12649f07ad3a
HernanQuijano <hernan.quijano@unillanos.edu.co>
2023-12-11 03:46:18 -0500

    modify navbar and restring sesion

--- Frontend/Scripts/index-Login.js
+++ Frontend/Scripts/index-Login.js
@@ -40,3 +40,15 @@ function Login() {
         $("#form-modal-login").html("Error al iniciar sesión. " + error.message);
     });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    var registerLink = document.getElementById("login-button-registrer");
+
+    registerLink.addEventListener("click", function (event) {
+      $('#modal-Login').modal('hide');
+      
+      $('#modal-Register').modal('show');
+
+      event.preventDefault();
+    });
+  });
